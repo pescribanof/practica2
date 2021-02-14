6da0fcca437d50d63df2080209f73718de7f6e58
pescribanof <76837868+pescribanof@users.noreply.github.com>
2021-02-14 12:29:28 +0100

    Add files via upload

---
+++ two-vms/network.tf
@@ -0,0 +1,24 @@
+# Creación de red
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+
+resource "azurerm_virtual_network" "myNet" {
+    name                = "kubernetesnet_pef"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    tags = {
+        environment = "CP2"
+    }
+}
+
+# Creación de subnet
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
+
+resource "azurerm_subnet" "mySubnet" {
+    name                   = "terraformsubnet_pef"
+    resource_group_name    = azurerm_resource_group.rg.name
+    virtual_network_name   = azurerm_virtual_network.myNet.name
+    address_prefixes       = ["10.0.1.0/24"]
+
+}
