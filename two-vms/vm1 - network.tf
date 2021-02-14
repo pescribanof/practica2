ae4faf6fb51d63348cf523f94f30afa604c1b272
pescribanof <76837868+pescribanof@users.noreply.github.com>
2021-02-14 12:30:20 +0100

    Add files via upload

---
+++ two-vms/vm1 - network.tf
@@ -0,0 +1,37 @@
+
+# Create NIC
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNic1" {
+  name                = "vmnic1_pef"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "myipconfiguration1_pef"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.11"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp1.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIp1" {
+  name                = "vmip1_pef"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+}
---
+++ two-vms/vm2 - network.tf
@@ -0,0 +1,36 @@
+# Create NIC
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNic2" {
+  name                = "vmnic2_pef"  
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "myipconfiguration2_pef"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.12"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp2.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIp2" {
+  name                = "vmip2_pef"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+}
