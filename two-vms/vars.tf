f3457119b593d57e1fd6619e4e7b64eaaeb4a6b7
pescribanof <76837868+pescribanof@users.noreply.github.com>
2021-02-21 13:26:41 +0100

    Add files via upload

--- two-vms/vars.tf
+++ two-vms/vars.tf
@@ -7,5 +7,5 @@ variable "location" {
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  default = "Standard_D2_v2" # 7 GB, 2 CPU 
 }
